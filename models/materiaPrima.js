357886f84df6f26e7789bbfa478f134594010a65
hlyras <hhlyras2011@gmail.com>
2015-12-11 21:42:14 -0200

    alterar e excluir

--- models/materiaPrima.js
+++ models/materiaPrima.js
@@ -5,15 +5,27 @@ var MateriaPrima = function(){
 
   this.salvar = function(callback){
     //db.cnn.exec("insert into materia_prima(id, nome) values("+ this.id +",'"+ this.nome +"')", callback)
-    db.cnn.exec("insert into materia_prima(nome) values('"+this.nome+"')", callback)
+    db.cnn.exec("insert into materia_prima(nome) values('"+ this.nome +"')", callback);
   }
+
   this.alterar = function(callback){
-    db.cnn.exec("update materia_prima set nome='"+ this.nome +"' where id="+this.id);
+    db.cnn.exec("update materia_prima set nome='"+ this.nome +"' where id="+this.id, callback);
+  }
+
+  this.excluir = function(callback){
+    console.log("=== this.id ===")
+    console.log(this.id)
+    //db.cnn.exec("update materia_prima set nome='"+ this.nome +"' where id="+this.id, callback);
+    db.cnn.exec("delete from materia_prima where id="+this.id, callback);
   }
 }
 
 MateriaPrima.todos = function(callback){
   db.cnn.exec("select * from materia_prima", callback);
+}
+
+MateriaPrima.buscaPorId = function(id, callback){
+  db.cnn.exec("select * from materia_prima where id="+id, callback);
 }
 
 module.exports = MateriaPrima;
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,13 @@ var router = express.Router();
 var MateriaPrima = require('../models/materiaPrima');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 
 router.post('/salvar', function(req, res, next) {
   var materiaPrima = new MateriaPrima();
-  materiaPrima.nome = req.body.nome; // nome
-
+  materiaPrima.nome = req.body.nome;
   materiaPrima.salvar(function(rows, err){
+    //se der tudo certo ele irá exe. a function e chamará de volta o end. '/'
     if(err){
       req.send("Erro ao salvar", 500);
     }
@@ -21,17 +19,72 @@ router.post('/salvar', function(req, res, next) {
   });
 });
 
-router.get('/lista', function(req, res, next){
+router.get('/', function(req, res, next){
 	MateriaPrima.todos(function(rows, err){
 		if(err) res.send("Erro ao buscar", 500);
 		else{
-			res.render('tabela',{
+			res.render('index',{
 				materias: rows
-			})
+			});
 		}
-	})
+	});
+});
+
+router.get('/editar', function(req, res, next){
+  MateriaPrima.buscaPorId(req.query.id, function(rows, err){
+    if(err) res.send("Erro ao buscar", 500);
+    else{
+      if(rows.length > 0){
+        var c = rows;
+        res.render('editar',{
+          title: 'Alterar Materia',
+          materia: c[0]
+        });
+      }
+    }
+  });
+});
+
+router.post('/alterar', function(req, res, next){
+  var materiaPrima = new MateriaPrima();
+  materiaPrima.nome = req.body.nome;
+  materiaPrima.id = req.body.id;
+  materiaPrima.alterar(function(rows, err){
+    if(err){
+      req.send("Erro ao alterar", 500);
+    }
+    else{
+      res.redirect('/')
+    }
+  })
 })
 
+router.get('/buscar', function(req, res, next){
+  MateriaPrima.buscaPorId(req.query.id, function(rows, err){
+    if(err) res.send("Erro ao buscar", 500);
+    else{
+      if(rows.length > 0){
+        var c = rows;
+        res.render('excluir',{
+          title: 'Excluir Materia',
+          materia: c[0]
+        });
+      }
+    }
+  });
+});
 
+router.post('/excluir', function(req, res, next){
+  var materiaPrima = new MateriaPrima();
+  materiaPrima.id = req.body.id;
+  materiaPrima.excluir(function(rows, err){
+    if(err){
+      req.send("Erro ao alterar", 500);
+    }
+    else{
+      res.redirect('/')
+    }
+  })
+})
 
 module.exports = router;
